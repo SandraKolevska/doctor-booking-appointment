28fb4db3e7343c6209ecbf41b1bd28d6dc177ff3
Filip Kuzmanoski <filipkuzmanoski04@gmail.com>
2025-03-21 23:55:16 +0100

    add logo , viteconfig updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
+// filepath: c:\Users\Филип Кузманоски\Desktop\Doctor-Booking-Appointment-\frontend\vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  }
 })
